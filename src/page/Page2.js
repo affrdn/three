d453934dd5eef94e299e69a861fec2bf53501a98
aff <afifrmdn@gmail.com>
2024-10-22 02:45:57 +0700

    first commit

--- src/page/Page2.js
+++ src/page/Page2.js
@@ -16,14 +16,14 @@ const Page2 = () => {
           </div>
 
           <div className="lg:w-1/2">
-            <div className="bg-white text-[#43520d] font-extrabold rounded-lg -rotate-2 py-4 mb-6">
-              <h1 className="text-4xl lg:text-5xl text-start font-bold ml-4">
+            <div className="bg-white text-[#43520d] font-extrabold rounded-lg -rotate-2 py-4 mb-6 border-solid border-8 border-t-2 border-e-2 border-[#b6c769]">
+              <h1 className="text-4xl lg:text-5xl text-start font-bold ml-4 ">
                 #ABOUT APUGROOT
               </h1>
             </div>
 
-            <div className="bg-white text-[#43520d] font-extrabold rounded-xl pb-12 lg:text-3xl text-2xl h-1/4 py-4 px-1.5">
-              <span>
+            <div className="bg-white text-[#43520d] font-extrabold rounded-xl pb-12 lg:text-3xl text-2xl h-1/4 py-4 px-1.5 border-solid border-8 border-s-2 border-e-4 border-[#b6c769] ">
+              <span className=" border-solid border-2 border-r border-l border-t border-[#b6c769] ">
                 Meet $APUGROOT one of apu apustaja best frens a sentient
                 tree-like creature who communicates primarily through the phrase
                 "I am Groot." living together with harmony in Apu Town
